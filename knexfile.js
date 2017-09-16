ef1dde9962d8c681488b030153397ea28a3b9efc
ariflo <arian@jurispect.com>
2017-09-16 14:23:40 -0700

    restructure

--- knexfile.js
+++ knexfile.js
@@ -29,7 +29,7 @@ module.exports = {
   production: {
     client: 'postgresql',
     connection: {
-      database: 'my_db',
+      database: 'anr_db',
       user:     'username',
       password: 'password'
     },
