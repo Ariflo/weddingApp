7b61ef65ae2ddde055afb78359a8b1954790c1f4
ariflo <arian@jurispect.com>
2017-10-24 21:59:37 -0700

    adds more debug tool

---
+++ web.server.prod.js
@@ -0,0 +1,36 @@
+/* eslint-disable no-console */
+
+const compression = require('compression');
+const express = require('express');
+const favicon = require('serve-favicon');
+const path = require('path');
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.use((req, resp, next) => {
+  if (req.headers['x-forwarded-proto'] !== 'https') {
+    return resp.redirect(['https://', req.get('Host'), req.url].join(''));
+  }
+  return next();
+});
+
+
+app.use(compression());
+app.use(favicon(`${__dirname}/dist/favicon.ico`));
+
+app.use('/', (req, res, next) => {
+  express.static(`${__dirname}/dist/app`)(req, res, next);
+});
+
+app.get('*', function (req, res, next) {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, err => {
+  if (err) {
+    console.error(`Error starting server: ${err}`);
+    return;
+  }
+  console.log('Server is listening');
+});
