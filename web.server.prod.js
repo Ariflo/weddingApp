47188d8e6796ad4b637cf2d4389d187864a29b42
ariflo <arian@jurispect.com>
2017-11-07 11:04:43 -0800

    deploy issues

--- web.server.prod.js
+++ web.server.prod.js
@@ -17,13 +17,13 @@ app.use((req, resp, next) => {
 
 
 app.use(compression());
-app.use('/', (req, res, next) => {
-  express.static(`${__dirname}/dist/app`)(req, res, next);
-});
 app.use(favicon(`${__dirname}/dist/favicon.ico`));
+app.use('/', (req, res, next) => {
+  express.static(`${__dirname}/web/public`)(req, res, next);
+});
 
 app.get('*', function (req, res, next) {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'web/public', 'index.html'));
 });
 
 app.listen(PORT, err => {
