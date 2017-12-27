4fed1042d1f4d48079d95bf4fc12095a70aa1953
AriFlo <ariflo@arianflores.com>
2017-12-26 19:26:49 -0800

    config updates

--- server/api.js
+++ server/api.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import methodOverride from 'method-override';
-import bodyParser from 'body-parser'
+import bodyParser from 'body-parser';
 import { Pool, Client } from 'pg';
 import router from './controllers/router';
-import config from './config'
+import config from './config';
 
 // load dotenv
 require('dotenv').load();
@@ -15,17 +15,17 @@ const client = new Client(config.db);
 pool.connect(function(err, client, release) {
   if (err) throw err;
   console.log('Postgres Connected!');
-  client.query('SELECT * from "Guests"', (err, result) => {
-  release()
-  if (err) {
-    return console.error('Error executing query', err.stack)
-  }
-  console.log(result.rows);
-})
-})
+  client.query(`SELECT * FROM "Guests"`, (err, result) => {
+    release();
+    if (err) {
+      return console.error('Error executing query', err.stack);
+    }
+    console.log(result.rows);
+  });
+});
 
 // pool shutdown
-pool.end()
+pool.end();
 
 //create server instance
 const server = express();
@@ -38,7 +38,6 @@ server.use(bodyParser.urlencoded({ extended: false }));
 API ROUTES HERE
 *****************************/
 server.use('/guests', router.guests);
-
 
 //set a port to listen to
 const port = config.port || 8080;
