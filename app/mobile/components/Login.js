d4bc80679959a606b8ba2da6b64e9e161d92aea2
AriFlo <ariflo@arianflores.com>
2017-12-27 16:14:54 -0800

    applies styles

--- app/mobile/components/Login.js
+++ app/mobile/components/Login.js
@@ -1,36 +1,54 @@
 import React, { Component } from 'react';
-import { Container, Header, Content, Form, Item, Input, Label, Button, Text } from 'native-base';
+import { ImageBackground, Platform } from 'react-native';
+import {
+  Container,
+  Header,
+  Content,
+  Form,
+  Item,
+  Input,
+  Label,
+  Button,
+  Text,
+  StyleProvider
+} from 'native-base';
 import { connect } from 'react-redux';
 import login from '../styles/components/login_styles.js';
-
+import { getStyleTheme, native_base_material } from '../styles/modules/main-styles.js';
 
 class Login extends Component {
   render() {
     return (
-      <Container>
-        <Content>
-          <Form>
-            <Item floatingLabel>
-              <Label>Username</Label>
-              <Input />
-            </Item>
-            <Item floatingLabel>
-              <Label>Password</Label>
-              <Input />
-            </Item>
-          </Form>
-          <Button style={login.sign_up_btn} bordered >
-            <Text>Sign-in</Text>
-          </Button>
-        </Content>
-      </Container>
+      <StyleProvider style={getStyleTheme(native_base_material)}>
+        <Container>
+          <ImageBackground
+            source={{
+              uri: 'https://media.giphy.com/media/rIDbVBecGulqM/giphy.gif',
+              cache: 'default'
+            }}
+            style={{ width: null, height: 620 }}
+          >
+            <Content style={login.content_container}>
+              <Form>
+                <Item floatingLabel last>
+                  <Label>Guest Code</Label>
+                  <Input secureTextEntry style={login.input} />
+                </Item>
+              </Form>
+              <Button style={login.sign_up_btn}>
+                <Text style={login.sign_up_btn_txt}>Sign-in</Text>
+              </Button>
+            </Content>
+          </ImageBackground>
+        </Container>
+      </StyleProvider>
     );
   }
 }
 
 const mapStateToProps = ({ first_store }) => {
   return {
-    first_store,
+    first_store
   };
 };
 
--- app/mobile/styles/components/login_styles.js
+++ app/mobile/styles/components/login_styles.js
@@ -1,14 +1,24 @@
 import { StyleSheet } from 'react-native';
 
-const login = StyleSheet.create({
-  label: {
-    color: 'black',
+export default login = StyleSheet.create({  
+  content_container: {
+    marginTop: 40,
   },
   
   sign_up_btn: {
     marginTop: 20,
     alignSelf: 'center',
+    borderColor: '#0d3b4c',
+    backgroundColor: 'transparent',
+    borderWidth: .4,
+    
   },
+  
+  sign_up_btn_txt: {
+    color: '#0d3b4c',
+  },
+  
+  input: {
+    color: '#0d3b4c'
+  }
 });
-
-export default login;
