cfecd3284df710b6a0ad999333d1b47c4d804411
AriFlo <ariflo@arianflores.com>
2017-12-29 12:56:43 -0800

    adds contruction view

---
+++ app/mobile/styles/components/construction_styles.js
@@ -0,0 +1,28 @@
+import { StyleSheet } from 'react-native';
+
+export default login = StyleSheet.create({  
+  container: {
+    backgroundColor: '#d2dbe2',
+    flex: 1,
+    alignItems: "center",
+    flexDirection: "column",
+    justifyContent: "center",
+  },
+  
+  text_container: {
+    marginTop: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    paddingHorizontal: 45,
+  },
+  
+  txt: {
+    textAlign: 'center',
+    fontSize: 18,
+  },
+  
+  thumbnail: {
+    width: 200,
+    height: 220,
+  },
+});
