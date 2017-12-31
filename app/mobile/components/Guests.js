f6e4a53bd225b8848c799b2a8293411fbdb83458
AriFlo <ariflo@arianflores.com>
2017-12-31 02:51:18 -0800

    adds more fixes

--- app/mobile/components/Guests.js
+++ app/mobile/components/Guests.js
@@ -4,27 +4,27 @@ import { ScrollView } from 'react-native';
 import guests from '../styles/components/guests_styles.js';
 import { Container, Header, Content, List, ListItem, Text } from 'native-base';
 import { fetchGuests } from '../../shared/actions';
+import { Actions } from 'react-native-router-flux';
 
 class Guests extends Component {
   constructor(props) {
     super(props);
-
+    
     this.state = {};
   }
 
   componentWillMount() {
     this.props.fetchGuests();
   }
-  
+
   render() {
     const divider_letters = {};
-    
     const guest_list = _.sortBy(this.props.all_guests, 'first_name').reduce(
       (mem, guest) => {
-        if (!divider_letters[guest.first_name[0]]) {
+        if (typeof guest === 'object' && !divider_letters[guest.first_name[0]]) {
           divider_letters[guest.first_name[0]] = true;
           const unique_key = guest.attendee_id + Math.random() * 10000;
-          
+
           mem.push(
             <ListItem key={unique_key} itemDivider>
               <Text>{guest.first_name[0]}</Text>
