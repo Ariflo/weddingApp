63ef4bcc34811375b42ed85bf075a052cab69f5c
AriFlo <ariflo@arianflores.com>
2017-12-30 19:38:31 -0800

    adds action to add so

--- app/mobile/components/Invite.js
+++ app/mobile/components/Invite.js
@@ -4,14 +4,14 @@ import { Actions } from 'react-native-router-flux';
 import Modal from 'react-native-modal';
 import { Container, Content, Header, Item, Input, Button, Text } from 'native-base';
 import { connect } from 'react-redux';
-import { add_guest } from '../../shared/actions';
+import { add_guest, add_significant_other } from '../../shared/actions';
 import invite from '../styles/components/invite_styles.js';
 
 class Invite extends Component {
   constructor(props) {
     super(props);
 
-    this.default_state = {
+    this.state = {
       guest_data: {
         guest_id: null,
         first_name: '',
@@ -24,40 +24,37 @@ class Invite extends Component {
       plus_one_open: false,
       plus_kids: false
     };
-
-    this.state = this.default_state;
   }
 
   handleChange(field, value) {
     const new_state = { guest_data: this.state.guest_data };
     new_state.guest_data[field] = value;
-
     this.setState(new_state);
   }
 
   addPlusOne(add_plus_one) {
     if (!add_plus_one) {
       if (this.state.plus_kids) {
-        this.setState(this.default_state);
+        this._resetState();
         Actions.guests();
       } else {
+        this._resetState();
         this.setState({
-          ...this.default_state,
           plus_kids: true
         });
       }
     } else {
       if (this.state.guest_type === 'Guest') {
+        this._resetState();
         this.setState({
-          ...this.default_state,
           guest_type: 'Significant Other',
           guest_data: {
             guest_id: this.state.guest_data.guest_id
           }
         });
       } else {
+        this._resetState();
         this.setState({
-          ...this.default_state,
           guest_type: 'Child',
           guest_data: {
             guest_id: this.state.guest_data.guest_id
@@ -79,14 +76,12 @@ class Invite extends Component {
         });
       });
     } else if (this.state.guest_type === 'Significant Other') {
-      console.log(this.state.guest_data);
-      //this.props.add_significant_other(this.state.guest_data);
+      this.props.add_significant_other(this.state.guest_data);
       this.setState({
         plus_one_open: true,
         plus_kids: true
       });
     } else if (this.state.guest_type === 'Child') {
-      console.log(this.state.guest_data);
       //this.props.add_child(this.state.guest_data);
       this.setState({
         plus_one_open: true,
@@ -94,7 +89,23 @@ class Invite extends Component {
       });
     }
   }
-
+  
+  _resetState() {
+    this.setState({
+      guest_data: {
+        guest_id: null,
+        first_name: '',
+        last_name: '',
+        email: '',
+        phone: '',
+        address: ''
+      },
+      guest_type: 'Guest',
+      plus_one_open: false,
+      plus_kids: false
+    });
+  }
+  
   render() {
     const any_more_kids = this.state.guest_type === 'Child' ? 'another' : 'a' ;
     return (
@@ -185,5 +196,6 @@ const mapStateToProps = state => {
 };
 
 export default connect(mapStateToProps, {
-  add_guest
+  add_guest,
+  add_significant_other
 })(Invite);
--- app/shared/constants.js
+++ app/shared/constants.js
@@ -2,3 +2,5 @@ export const ADD_GUEST = "ADD_GUEST";
 export const GUEST_ADDED = "GUEST_ADDED";
 export const GUEST_LOGIN = "GUEST_LOGIN";
 export const GUEST_LOGGED_IN = "GUEST_LOGGED_IN";
+export const ADD_SO = "ADD_SO";
+export const SO_ADDED = "SO_ADDED";
