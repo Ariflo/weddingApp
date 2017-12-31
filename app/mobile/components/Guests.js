9f689d0306efc8bd2c985cb03b99318482f8ce75
AriFlo <ariflo@arianflores.com>
2017-12-31 01:33:38 -0800

    adds scrolling list

--- app/mobile/components/Guests.js
+++ app/mobile/components/Guests.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { ScrollView } from 'react-native';
 import guests from '../styles/components/guests_styles.js';
-import { Container, Header, Content, List, ListItem, Text} from 'native-base';
+import { Container, Header, Content, List, ListItem, Text } from 'native-base';
 import { fetchGuests } from '../../shared/actions';
 
 class Guests extends Component {
@@ -11,36 +11,43 @@ class Guests extends Component {
 
     this.state = {};
   }
-  
+
   componentWillMount() {
     this.props.fetchGuests();
   }
 
   render() {
-    console.log(this.props.all_guests);
+    const divider_letters = {};
+    const guest_list = _.sortBy(this.props.all_guests, 'first_name').reduce(
+      (mem, guest) => {
+        if (!divider_letters[guest.first_name[0]]) {
+          divider_letters[guest.first_name[0]] = true;
+          const unique_key = guest.attendee_id + Math.random() * 10000;
+          mem.push(
+            <ListItem key={unique_key} itemDivider>
+              <Text>{guest.first_name[0]}</Text>
+            </ListItem>
+          );
+        }
+        mem.push(
+          <ListItem key={guest.attendee_id}>
+            <Text>
+              {guest.first_name} {guest.last_name}
+            </Text>
+          </ListItem>
+        );
+
+        return mem;
+      },
+      []
+    );
     return (
       <Container>
         <Header style={guests.header}>
           <Text style={guests.header_txt}>Guest List</Text>
         </Header>
         <Content>
-          <List>
-            <ListItem itemDivider>
-              <Text>A</Text>
-            </ListItem>
-            <ListItem>
-              <Text>Aaron Bennet</Text>
-            </ListItem>
-            <ListItem>
-              <Text>Ali Connors</Text>
-            </ListItem>
-            <ListItem itemDivider>
-              <Text>B</Text>
-            </ListItem>
-            <ListItem>
-              <Text>Bradley Horowitz</Text>
-            </ListItem>
-          </List>
+          <List>{guest_list}</List>
         </Content>
       </Container>
     );
