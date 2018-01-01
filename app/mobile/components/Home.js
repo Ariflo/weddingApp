0cca7c8c3e1ddbc4a68acf539f6cd98a47940b5e
AriFlo <ariflo@arianflores.com>
2017-12-31 23:39:19 -0800

    styles home page

--- app/mobile/components/Home.js
+++ app/mobile/components/Home.js
@@ -1,19 +1,48 @@
 import React, { Component } from 'react';
+import { View } from 'react-native';
 import { connect } from 'react-redux';
 import { ScrollView } from 'react-native';
-import { Container, Content } from 'native-base';
+import { Container, Content, Text } from 'native-base';
+import home from '../styles/components/home_styles.js';
 
 class Home extends Component {
   constructor(props) {
     super(props);
-
     this.state = {};
   }
 
   render() {
     return (
       <Container>
-        <Content />
+        <Content contentContainerStyle={home.container}>
+          <Text style={home.date}>January 17, 2019</Text>
+          <View style={home.countdown}>
+            <View style={home.countdown_num_container}>
+              <View style={home.date_box}>
+                <Text style={home.date_txt}>200</Text>
+              </View>
+              <Text style={home.date_title}>DAYS</Text>
+            </View>
+            <View style={home.countdown_num_container}>
+              <View style={home.date_box}>
+                <Text style={home.date_txt}>10</Text>
+              </View>
+              <Text style={home.date_title}>HOURS</Text>
+            </View>
+            <View style={home.countdown_num_container}>
+              <View style={home.date_box}>
+                <Text style={home.date_txt}>51</Text>
+              </View>
+              <Text style={home.date_title}>MINUTES</Text>
+            </View>
+            <View style={home.countdown_num_container}>
+              <View style={home.date_box}>
+                <Text style={home.date_txt}>10</Text>
+              </View>
+              <Text style={home.date_title}>SECONDS</Text>
+            </View>
+          </View>
+        </Content>
       </Container>
     );
   }
